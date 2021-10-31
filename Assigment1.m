ebaeff141051352745391a1373e73c579afe626d
Mohamed nofal <93469459+mo-nofal@users.noreply.github.com>
2021-10-31 20:35:55 +0300

    Add files via upload

---
+++ Assigment1.m
@@ -0,0 +1,24 @@
+A=[-7 5 -9; 2 -1 2; 1 -1 2];
+B=[16 3 2 13;5 10 11 8;9 6 7 12 ;4 15 14 1];
+C=[4 2 -3;7 -7 9;3 -5 6];
+D=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
+T=3*A-5*C;
+%7*A+2*B there is an error becouse the number of rows in matrix of A not equale the matrix of B%
+r=C*A;
+j=C*D';
+zeros(4,2)
+zeros (4)
+ones(6);
+ones(4,4);
+size (D);
+zeros (size(D));
+diag([1 2 3 4]);
+eye(7);
+X=[2,5];
+x=[2;5];
+h=diag([5 5 5 5 5 5 5 ]);
+h(:,8)=[5 5 5 5 5 5 5 ];
+S=A(3,:);
+G=A(:,2);
+
+
